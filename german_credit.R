bd55a2e2227c516a7876413856fdd449a037e88f
Joshua Prince <joshua.prince@modelop.com>
2023-08-09 15:39:05 -0600

    fixed metrics data and function

--- german_credit.R
+++ german_credit.R
@@ -4,23 +4,25 @@ library(readr)
 
 # modelop.init
 begin <- function() {
-    # model <- read_rds("trained_model.rds")
-    load("trained_model.RData")
-    model <<- logreg_fit
+  # model <- read_rds("trained_model.rds")
+  load("trained_model.RData")
+  model <<- logreg_fit
 }
 
 # modelop.score
 action <- function(data) {
-    df <- data.frame(data, stringsAsFactors = F)
-    preds <- predict(model, df)
-    output <- list(label_value = df$label, score = preds$.pred_class)
-    emit(output)
+  df <- data.frame(data, stringsAsFactors = F)
+  preds <- predict(model, df)
+  output <- list(label_value = df$label, score = preds$.pred_class)
+  emit(output)
 }
 
 # modelop.metrics
 metrics <- function(data) {
-    df <- data.frame(data)
-    get_metrics <- metric_set(f_meas, accuracy, sensitivity, specificity, precision)
-    output <- get_metrics(data = df, truth = as.factor(label_value), estimate = as.factor(score))
-    emit(output)
+  df <- data.frame(data)
+  df$label_value <- as.factor(df$label_value)
+  df$score <- as.factor(df$score)
+  get_metrics <- metric_set(f_meas, accuracy, sensitivity, specificity, precision)
+  output <- get_metrics(data = df, truth = label_value, estimate = score)
+  emit(output)
 }
